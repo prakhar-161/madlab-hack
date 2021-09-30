3a2c42befccdf4bf7860b940d03d40994b64e075
Prakhar Dhama <pdhama161@gmail.com>
2021-10-01 00:44:23 +0530

    Update fibonacci_using_recursion.cpp

--- hacktoberfest2021/fibonacci_using_recursion.cpp
+++ hacktoberfest2021/fibonacci_using_recursion.cpp
@@ -5,13 +5,28 @@ using namespace std;
 Task:
 1. WAP tp print fibonacci series till n using recursion
 */
-void fibonacci(int n){
-   if (n <= 1)
-        return n;
-    return fib(n-1) + fib(n-2);
-    
+
+void printFiboSeries(int n)
+{
+    int a = 0, b = 1, i;
+ 
+    if (n < 1)
+        return;
+    cout << a << " ";
+    for (i = 1; i < n; i++) {
+        cout << b << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
 }
-int main(){
-    fibonacci(10);
+
+int main()
+{
+    int n;
+    cout << "Enter value of n - ";
+    cin >> n;
+
+    printFiboSeries(n);
     return 0;
 }
